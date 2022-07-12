6c39d2ac7c644b0301c1ae454dc54075089fa5ec
emrebaspinar1605 <turkuz-elhamdulillah@hotmail.com>
2022-07-12 15:22:23 +0300

    HackerRank Solution

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Printer
+{
+       public static void PrintArray<T>(T[] arr)
+        {
+            foreach (var element in arr)
+            {
+                Console.WriteLine(element);
+            }
+        }
+	/**
+	*    Name: PrintArray
+	*    Print each element of the generic array on a new line. Do not return anything.
+	*    @param A generic array
+	**/
+    // Write your code here
+
+
+    static void Main(string[] args)
+	{
+		int n = Convert.ToInt32(Console.ReadLine());
+		int[] intArray = new int[n];
+		for (int i = 0; i < n; i++)
+		{
+			intArray[i] = Convert.ToInt32(Console.ReadLine());
+		}
+		
+		n = Convert.ToInt32(Console.ReadLine());
+		string[] stringArray = new string[n];
+		for (int i = 0; i < n; i++)
+		{
+			stringArray[i] = Console.ReadLine();
+		}
+		
+		PrintArray<Int32>(intArray);
+		PrintArray<String>(stringArray);
+	}
+}
